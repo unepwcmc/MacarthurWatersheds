77922ccba18d817c637ac71e769183271cd4ddbd
James Cox <th3james@gmail.com>
2014-03-06 12:58:41 +0000

    Remove sample test file

--- js/tests.js
+++ js/tests.js
@@ -1,11 +1,4 @@
 (function() {
-  test('the truth', function() {
-    return assert.isTrue(true);
-  });
-
-}).call(this);
-
-(function() {
   suite('Perspective View');
 
   test("render shows the title 'perspective'", function() {
