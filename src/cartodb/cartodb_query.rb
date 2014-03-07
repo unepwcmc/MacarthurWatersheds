cabb5c1aae0a0e2de23b8ee923d647da7f009ad6
migtorres <miguel.torres@unep-wcmc.org>
2014-03-07 13:54:06 +0000

    importing all except EF

--- src/cartodb/cartodb_query.rb
+++ src/cartodb/cartodb_query.rb
@@ -6,7 +6,9 @@ class CartodbQuery
     cartodb_config = YAML.load_file('../config/cartodb_config.yml')
     api_key = cartodb_config["api_key"]
     host = cartodb_config["host"]
+
     encoded_url = URI::encode("#{host}/api/v2/sql?q=#{sql}&api_key=#{api_key}")
+    puts encoded_url
     result = open(encoded_url).read
   end
 end
