6b8c8a0ee342466e38236bcde921dae848248417
James Cox <th3james@gmail.com>
2014-03-07 16:17:26 +0000

    Add some controller tests

--- coffee/tests/controllers/main.coffee
+++ coffee/tests/controllers/main.coffee
@@ -1,1 +1,31 @@
 suite 'Main Controller'
+
+test('The application starts by showing the choose region view', ->
+  chooseRegionActionStub = sinon.stub(Backbone.Controllers.MainController::, "chooseRegion", ->)
+  controller = new Backbone.Controllers.MainController()
+
+  try
+    assert.isTrue chooseRegionActionStub.calledOnce, "Expected the chooseRegion action to be called"
+  finally
+    chooseRegionActionStub.restore()
+)
+
+test('From the choose region view, if I pick a region, it transitions to the show action', ->
+  showActionStub = sinon.stub(Backbone.Controllers.MainController::, 'show', ->)
+  controller = new Backbone.Controllers.MainController()
+
+  chooseRegionView = controller.modalContainer.view
+
+  try
+    assert.isNotNull(chooseRegionView,
+      "Expected the controller to have a modal with choose region in")
+
+    chooseRegionView.$el.find('.regions li:first').trigger('click')
+
+    assert.isTrue showActionStub.calledOnce, "Expected the show action to be initialized"
+
+    controller.modalContainer.hideModal()
+
+  finally
+    showActionStub.restore()
+)
--- js/tests.js
+++ js/tests.js
@@ -1,6 +1,32 @@
 (function() {
   suite('Main Controller');
 
+  test('The application starts by showing the choose region view', function() {
+    var chooseRegionActionStub, controller;
+    chooseRegionActionStub = sinon.stub(Backbone.Controllers.MainController.prototype, "chooseRegion", function() {});
+    controller = new Backbone.Controllers.MainController();
+    try {
+      return assert.isTrue(chooseRegionActionStub.calledOnce, "Expected the chooseRegion action to be called");
+    } finally {
+      chooseRegionActionStub.restore();
+    }
+  });
+
+  test('From the choose region view, if I pick a region, it transitions to the show action', function() {
+    var chooseRegionView, controller, showActionStub;
+    showActionStub = sinon.stub(Backbone.Controllers.MainController.prototype, 'show', function() {});
+    controller = new Backbone.Controllers.MainController();
+    chooseRegionView = controller.modalContainer.view;
+    try {
+      assert.isNotNull(chooseRegionView, "Expected the controller to have a modal with choose region in");
+      chooseRegionView.$el.find('.regions li:first').trigger('click');
+      assert.isTrue(showActionStub.calledOnce, "Expected the show action to be initialized");
+      return controller.modalContainer.hideModal();
+    } finally {
+      showActionStub.restore();
+    }
+  });
+
 }).call(this);
 
 (function() {
