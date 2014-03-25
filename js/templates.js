277a36e634ef03249258d95f6f21fb5a3a405267
Blanca Tortajada <tortajadablanca@gmail.com>
2014-03-25 10:59:32 +0000

    added plugin for styling dropdows

--- js/templates.js
+++ js/templates.js
@@ -85,7 +85,7 @@ function program2(depth0,data) {
   buffer += "<h1>Lenses</h1>\n\n<select id=\"lens-select\">\n  ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.lenses), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</select>";
+  buffer += "\n</select>\n<span class=\"customSelect customSelectChanged customSelectOpen\">\n	<span class=\"customSelectInner\"></span>\n</span>";
   return buffer;
   });
 this["Handlebars"] = this["Handlebars"] || {};this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};this["Handlebars"]["templates"]["level_selector"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -286,12 +286,4 @@ function program1(depth0,data) {
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n</ul>\n";
   return buffer;
-  });
-this["Handlebars"] = this["Handlebars"] || {};this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};this["Handlebars"]["templates"]["scenario_selector"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  
-
-
-  return "<h1>ScenarioSelector View</h1>\n";
   });
