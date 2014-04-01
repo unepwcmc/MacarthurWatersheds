fe4a705cb3b3fd44e06663df1b9ef8b5208b99c3
Blanca Tortajada <tortajadablanca@gmail.com>
2014-04-01 11:51:43 +0100

    styling changes to make filters visible and add heirarchy

--- js/templates.js
+++ js/templates.js
@@ -102,7 +102,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n  <option value=\"";
+  buffer += "\n	  <option value=\"";
   if (stack1 = helpers.selector) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.selector); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
@@ -113,7 +113,7 @@ function program1(depth0,data) {
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.name); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</option>\n  ";
+    + "</option>\n	  ";
   return buffer;
   }
 function program2(depth0,data) {
@@ -122,14 +122,14 @@ function program2(depth0,data) {
   return "selected";
   }
 
-  buffer += "<h1>";
+  buffer += "<div class=\"selector\">\n	<h1>";
   if (stack1 = helpers.subject) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.subject); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + " Lens</h1>\n\n<select id=\"lens-select\" class='select-box'>\n  ";
+    + " Lens</h1>\n\n	<select id=\"lens-select\" class='select-box'>\n	  ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.lenses), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</select>";
+  buffer += "\n	</select>\n</div>\n";
   return buffer;
   });
 this["Handlebars"] = this["Handlebars"] || {};this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};this["Handlebars"]["templates"]["level_selector"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -297,12 +297,13 @@ function program3(depth0,data) {
   return buffer;
   }
 
-  buffer += "<h2><input type=\"checkbox\" name=\"protection\" ";
+  buffer += "<div class=\"secondary-filter\">\n	<h2><input type=\"checkbox\" name=\"See overlap with Protected Areas (PA)s\" ";
   stack1 = helpers['if'].call(depth0, (depth0 && depth0.protection), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += ">Protection</h2>\n\n";
+  buffer += ">See overlap with Protected Areas (PA)s</h2>\n</div>\n\n";
   stack1 = helpers['if'].call(depth0, (depth0 && depth0.protection), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
   return buffer;
   });
 this["Handlebars"] = this["Handlebars"] || {};this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};this["Handlebars"]["templates"]["protection_selector"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
