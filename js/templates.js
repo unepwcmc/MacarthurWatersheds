b6249ecf4739bc4897af4a35cd7b7ddfdae86c45
deciob <decio.battaglia@gmail.com>
2014-04-02 15:19:49 +0100

    small change of text

--- js/templates.js
+++ js/templates.js
@@ -182,7 +182,7 @@ function program2(depth0,data) {
   return "selected";
   }
 
-  buffer += "<h1>Level of Importance</h1>\n\n<select id=\"levels-select\" class='select-box'>\n  ";
+  buffer += "<h1>Level of change</h1>\n\n<select id=\"levels-select\" class='select-box'>\n  ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.levels), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n</select>\n";
