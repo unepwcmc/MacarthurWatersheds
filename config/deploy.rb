f8ae1bdb14ee0459bbcfbd03cdcb7f40ffc7ea23
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-08-24 13:57:50 +0100

    Added Linode Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,25 @@
-set :application, "Macarthur"
-set :user, "ubuntu"
-set :repository,  "https://github.com/unepwcmc/MacarthurWatersheds.git"
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory $
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, 'Macarthur'
+set :repo_url, 'git@github.com:unepwcmc/MacarthurWatersheds.git'
 
-role :web, "ec2-46-51-154-19.eu-west-1.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
-role :app, "ec2-46-51-154-19.eu-west-1.compute.amazonaws.com"                          # This may be the same as your `Web` server
-role :db,  "ec2-46-51-154-19.eu-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
+set :branch, 'LinodeDeploy'
 
-set :branch, "production"
-set :scm_username, "unepwcmc-read"
+set :deploy_user, 'wcmc'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+
+set :pty, true
 
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :ssh_options, {
+  forward_agent: true,
+}
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
-set :deploy_to, "/home/ubuntu/#{application}"
-set :normalize_asset_timestamps, false
 set :linked_dirs, %w{data/datasheets}
+
+set :keep_releases, 5
+
+set :passenger_restart_with_touch, false
+
+
---
+++ old_cap/deploy.rb
@@ -0,0 +1,33 @@
+set :application, "Macarthur"
+set :user, "ubuntu"
+set :repository,  "https://github.com/unepwcmc/MacarthurWatersheds.git"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory $
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "ec2-46-51-154-19.eu-west-1.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-46-51-154-19.eu-west-1.compute.amazonaws.com"                          # This may be the same as your `Web` server
+role :db,  "ec2-46-51-154-19.eu-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
+
+set :branch, "production"
+set :scm_username, "unepwcmc-read"
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+set :deploy_to, "/home/ubuntu/#{application}"
+set :normalize_asset_timestamps, false
+set :linked_dirs, %w{data/datasheets}
