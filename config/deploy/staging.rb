e8b6924c9e6a1efd5aa1a2fd2425b1dbdc2156dd
Stuart Watson <stuart.watson@unep-wcmc.org>
2017-12-14 10:14:21 +0000

    Added staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,13 @@
+set :stage, :staging
+
+server "web-staging.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
+
+set :application, "watersheds"
+set :server_name, "watersheds.web-staging.linode.unep-wcmc.org"
+set :sudo_user, "wcmc"
+set :app_port, "80"
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
